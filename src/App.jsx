5c7c572577b3bb415e4fb6a793b1b2919094770c
Demyrah Bivings <demyrahbivings@Demyrahs-iMac.local>
2024-02-12 19:36:50 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+import Blue from './components/Blue'
+import Red from './components/Red'
+import Home from './components/Home'
+import Footer from './components/Footer'
+
+function App() {
+  
+
+  return (
+<div id="container">
+      <div id="navbar">
+        <Link to="/blue">Blue</Link>
+        <Link to="/red">Red</Link>
+        <Link to="/home">Home</Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/blue" element={<Blue/>} />
+          <Route path="/red" element={<Red/>}/>
+          <Route path="/home" element={<Home/>}/>
+        </Routes>
+      </div>
+      <div>
+        <Footer />
+      </div>
+</div>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Red from "./Red";
+import Blue from "./Blue";
+import { Routes, Route, Link } from 'react-router-dom'
+
+export default function Footer() {
+    return (
+      <div className="footer">
+        
+      </div>
+    );
+  }
